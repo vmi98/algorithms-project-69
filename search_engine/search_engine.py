3837b4ad679a2c77cb81856afa4b3bbdf3d39228
VladaM <vladaa905@gmail.com>
2025-06-02 17:07:31 +0300

    add TF_IDF sorting

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,31 +1,67 @@
 import re
+import math
+from collections import defaultdict
 
 
 def clean_word(w):
     return ''.join(re.findall(r'\w+', w)).lower()
 
 
-def search(docs, word):
-    if not docs or not word:
+def clean_multiple_words(words):
+    return [clean_word(t) for t in re.findall(r'\w+', words.lower())]
+
+
+def create_index(docs, words):
+    index = defaultdict(list)
+
+    words_set = set(words)
+    doc_words_map = {doc['id']: set(doc['text']) for doc in docs}
+
+    for word in words_set:
+        for key, value in doc_words_map.items():
+            if word in value:
+                index[word].append(key)
+    return index
+
+
+def TF_IDF(index, docs, words):
+    result = []
+    for doc in docs:
+
+        doc_text = doc['text']
+        if not any(word in doc_text for word in words):
+            continue
+
+        acc = []
+        for word in words:
+            TF = doc['text'].count(word) / len(doc)
+            IDF = math.log(len(docs) / len(index[word]))
+            TF_IDF = TF*IDF
+            acc.append(TF_IDF)
+        result.append({'id': doc['id'], 'weight': sum(acc)})
+    return result
+
+
+def search(docs, words):
+    if not docs or not words:
         return []
 
-    term_word = clean_word(word)
-    docs_found = []
-    for doc in docs:
-        tokens = [
-            clean_word(t)
-            for t in re.findall(r'\w+', doc['text'].lower())
-        ]
-        if term_word in tokens:
-            docs_found.append(
-                {'doc_id': doc['id'],
-                 'word_repeat': tokens.count(term_word)}
-            )
-    sorted_docs_found = sorted(
-        docs_found,
-        key=lambda x: x['word_repeat'],
+    if len(words) < 2:
+        term_words = {clean_word(words)}
+    else:
+        term_words = clean_multiple_words(words)
+
+    term_docs = [
+        {'id': doc['id'], 'text': clean_multiple_words(doc['text'])}
+        for doc in docs
+    ]
+    index = create_index(term_docs, term_words)
+
+    weighted_docs = TF_IDF(index, term_docs, term_words)
+    sorted_docs = sorted(
+        weighted_docs,
+        key=lambda x: x['weight'],
         reverse=True
     )
-    relevant_docs = list(map(lambda doc: doc['doc_id'], sorted_docs_found))
+    relevant_docs = list(map(lambda doc: doc['id'], sorted_docs))
     return relevant_docs
-
